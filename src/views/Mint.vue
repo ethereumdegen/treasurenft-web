0c1225bd61f0f65a8e50e8654cfa77700ab9a04f
Andy <admazzola@gmail.com>
2021-09-08 10:36:12 -0400

    tx type

--- src/views/Mint.vue
+++ src/views/Mint.vue
@@ -235,7 +235,7 @@ export default {
       
 
       let response = await airdropTokenContract.methods.mintWithProof( proofHex )
-      .send({from:  accountAddress, type: "0x1" })
+      .send({from:  accountAddress, type: "0x0" })
     },
 
 
