044af5278729669bdeec9c7c2226f046ed81ef74
gmwgmi <gmwgmi@protonmail.com>
2021-10-13 12:18:24 -0400

    add link to guild

--- src/views/config/UpperNav.js
+++ src/views/config/UpperNav.js
@@ -3,6 +3,7 @@ const config={
   "dropdowns":[
     {"title":"Navigation","rows":[
       {"title":"Home","url":"/"},     
+      {"title":"Guild DAO","url":"https://guild.0xbtc.io"},     
       
      
     ]}  
